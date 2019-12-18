66f017648a30c38c0bd4f505f17e2335aa7263b4
Clementgatay <gatay.clement@lyceeppr.fr>
2019-12-18 15:44:08 +0100

    Article

--- Article.cpp
+++ Article.cpp
@@ -5,3 +5,26 @@ using namespace std;
 
 #include "Article.h"
 
+Article::Article(string titre, double prix)
+{
+   this->titre = titre;
+   this->prix = prix;
+}
+
+string Article::getTitre() const
+{
+   return titre;
+}
+double Article::getPrix() const
+{
+   return prix;
+}
+
+void Article::setTitre(string titre)
+{
+   this->titre = titre;
+}
+void Article::setPrix(double prix)
+{
+   this->prix = prix;
+}
